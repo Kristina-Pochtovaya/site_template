8f32a9107aabc1b74e2665a727fdc0a3c502de43
Kristina <pochtovaya.kristina@mail.ru>
2025-03-07 14:47:54 +0200

    slider fixes

--- src/js/index.js
+++ src/js/index.js
@@ -4,5 +4,8 @@ import 'slick-carousel'
 document.addEventListener('DOMContentLoaded', function () {
   $('.slider-content').slick({
     dots: true,
+    slidesToShow: 1,
+    centerMode: true,
+    variableWidth: false,
   })
 })
